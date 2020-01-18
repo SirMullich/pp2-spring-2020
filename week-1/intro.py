7d0090b7d7270aab2d1eb20b5076f25b536dfc38
daulet <daulet@gmail.com>
2020-01-18 11:49:34 +0600

    week1

---
+++ week-1/intro.py
@@ -0,0 +1,62 @@
+# keys = 1000 and 1024
+# values = list
+SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+            1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+
+def approximate_size(size, a_kilobyte_is_1024_bytes = True):
+    '''Convert a file size to human-readable form.
+
+    Keyword arguments:
+    size -- file size in bytes
+    a_kilobyte_is_1024_bytes -- if True (default), use multiples of 1024
+                                if False, use multiples of 1000
+
+    Returns: string
+
+    '''
+    if size < 0:
+        raise ValueError('number must be non-negative')
+
+    # muliple = 1000 or 1024
+    multiple = 1024 if a_kilobyte_is_1024_bytes else 1000
+
+    # traverse list
+    # ex: SUFFIXES[multiple] -> ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    # ex: multiple = 1000
+    for suffix in SUFFIXES[multiple]:
+        # 1) 10000000 / 1000 -> 10000
+        # 2) 10000 / 1000 -> 10
+        size /= multiple # size = size / multiple
+        if size < multiple:
+            return '{0:.1f} {1}'.format(size, suffix)
+
+    raise ValueError('number too large')
+
+def is_odd(num):
+    if num % 2 == 0:
+        return False
+    else:
+        return True
+
+def tenth(num):
+    if (num >= 90):
+        return "90-100"
+    elif (num >= 80):
+        return "80-90"
+    elif (num >= 70):
+        return "70-80"
+    else:
+        return "0-70"
+
+def greeting():
+    name = input('Please input your name: ')
+    return 'Welcome {}'.format(name)
+
+if __name__ == '__main__':
+    # print(approximate_size(10000000, False))
+    # print(approximate_size(1000000000000))
+    # print(is_odd(101))
+    # print(tenth(47))
+    # print(tenth(94))
+    greet1 = greeting()
+    print(greet1)
