ac7d7c9ddc4d8e68e43fe70ccf4725f03809e101
daulet <daulet@gmail.com>
2020-01-25 10:50:56 +0600

    sets

--- week-2/main.py
+++ week-2/main.py
@@ -45,13 +45,112 @@ def list_intro():
     my_list.append([2001, 2002, 2003, 2004])
     print(my_list)
 
+    my_list.append(90)
+    # [9, 10, 1000, 1, 90, 83, 98, 2001, 2002, 2003, 2004, [2001, 2002, 2003, 2004], 90]
+
+    print('Count of 90 in the list: {0}'.format(my_list.count(90)))
+
+    # 'in' keyword
+    print('in keyword: {0}'.format(90 in my_list))
+
+    # index
+    print('First index of 90 in the list: {0}'.format(my_list.index(90)))
+
+    # print(my_list.index(86534))  # exception
+
+    if 86534 in my_list:
+        print(my_list.index(86534))
+
+    # delete value at index 0
+    del my_list[0]
+    print(my_list)
+
+    # remove value 90 from list
+    my_list.remove(90)
+    print('List after removing 90: {0}'.format(my_list))
+
+    # pop
+    print('Last value from list: {0}'.format(my_list.pop()))
+    print(my_list)
+
+    print('Boolean context of my_list: {0}'.format(is_it_true(my_list)))
+    print('Boolean context of empty list: {0}'.format(is_it_true([])))
+
+def tuple_demo(): 
+    # immutable -- cannot be changed or modified
+    # tuples are read-only
+    my_tuple = (89, 1, 20, 100, 74)
+    print(my_tuple[2])
+
+    # my_tuple[2] = 1000 # raises exception
+    # print(my_tuple)
+
+    # x = 1
+    # y = 2
+    # z = 3
+    # a = 4
+
+    (x, y, z, a) = (1, 2, 3, 4)
+
+    print('x = {0}'.format(x))
+
+    (one, two) = range(1, 3)
+    print(two)
+
+    return (8, 9, 10)
+
+def set_demo():
+    # set in UNORDERED
+    my_set = {8, 9, 8, 8, 8, 10} # multiple 8's
+    print(my_set)
+
+    # list is ORDERED
+    my_list = [10, 10, 10, 1, 1, 1, 2]
+    my_set_2 = set(my_list)
+    print(my_set_2)
+
+    my_set_2.add(10)
+    my_set_2.add(100)
+    my_set_2.add(100)
+    print(my_set_2)
+
+    print('Length of my_set_2 is: {0}'.format(len(my_set_2)))
+
+    # same as extend method in list
+    my_set_2.update({10, 1, -4})
+    print(my_set_2)
+
+    my_set_2.discard(-10000)
+    #my_set_2.remove(-10000) # exception
+    print(my_set_2)
+
+    print(my_set_2.pop())
+    print(my_set_2)
+    my_set_2.clear()
+    print(is_it_true(my_set_2))
+
 
 if __name__ == '__main__':
     # print(is_it_true(3))
     # print(is_it_true('this is string'))
     # print(is_it_true(math.pi))
     # print(is_it_true(0.0))
-    list_intro()
+    # list_intro()
+
+    # (x, y, z) = tuple_demo()
+    # print('Returned value from tuple_demo is: {0}'.format(x))
+    set_demo()
 
 
 # print('Hello World')
+
+
+
+
+
+
+
+
+
+
+
